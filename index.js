f7c9ae8644c9c5265d747695633f00b7349b55e6
Vishak <wizvishak@gmail.com>
2016-08-24 00:19:25 +0530

    welcome home

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var fs = require('fs')
-var marked = require('marked')
+var fs = require('fs');
+var marked = require('marked');
 
-var markdown = fs.readFileSync('index.md').toString()
-var header = fs.readFileSync('header.html').toString()
-var footer = fs.readFileSync('footer.html').toString()
+var markdown = fs.readFileSync('index.md').toString();
+var header = fs.readFileSync('header.html').toString();
+var footer = fs.readFileSync('footer.html').toString();
 
-var html = marked(markdown)
+var html = marked(markdown);
 
-fs.writeFileSync('index.html', header + html + footer)
+fs.writeFileSync('index.html', header + html + footer);
 
-console.log('(•◡•)/\n')
+console.log('(•◡•)/\n');
